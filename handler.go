8d832486609ff1423424257792240ee8a684f10a
baojianxiao <baojianxiao@bg.saicfc>
2016-08-29 15:44:44 +0800

    change handler

--- handler.go
+++ handler.go
@@ -39,19 +39,29 @@ func InitConfig(conf *Cfg) *HandlerWrapper {
 func (hw *HandlerWrapper) ServeHTTP(resp http.ResponseWriter, req *http.Request) {
 	raddr := *hw.MyConfig.Raddr
 	path := req.URL.Path
-	if strings.LastIndex(path, ".html") > 0 ||
+	if path == "/" || strings.LastIndex(path, ".html") > 0 ||
 		strings.LastIndex(path, ".css") > 0 ||
 		strings.LastIndex(path, ".js") > 0 ||
 		strings.LastIndex(path, ".jpg") > 0 ||
 		strings.LastIndex(path, ".png") > 0 ||
 		strings.LastIndex(path, ".gif") > 0 ||
+		strings.LastIndex(path, ".woff") > 0 ||
+		strings.LastIndex(path, ".woff2") > 0 ||
+		strings.LastIndex(path, ".ttf") > 0 ||
 		strings.LastIndex(path, ".map") > 0 {
+		if path == "/" {
+			path = "/index.html"
+		}
 		fin, err := os.Open(*hw.MyConfig.Static + path)
 		defer fin.Close()
 		if err != nil {
 			hw.Forward(resp, req, raddr)
 		}
 		fd, _ := ioutil.ReadAll(fin)
+		//Content-Type:text/css;charset=utf-8
+		resp.Header().Add("Content-Type", req.Header.Get("Content-Type"))
+		resp.WriteHeader(200)
+		//req.Header.Set("Connection", "Keep-Alive")
 		resp.Write(fd)
 	} else {
 		hw.Forward(resp, req, raddr)
@@ -65,14 +75,13 @@ func (hw *HandlerWrapper) Forward(resp http.ResponseWriter, req *http.Request, r
 		logger.Println("dial tcp error", err)
 	}
 
-	err = connectProxyServer(connOut, raddr)
-	if err != nil {
-		logger.Println("connectProxyServer error:", err)
-	}
+	//e := connectProxyServer(connOut, raddr, req)
+	//if e != nil {
+	//	logger.Println("connectProxyServer error:", err)
+	//}
 
 	if req.Method == "CONNECT" {
-		b := []byte("HTTP/1.1 200 Connection Established\r\n" +
-			"Proxy-Agent: html5_Dev_KevinBao/" + Version + "\r\n\r\n")
+		b := []byte("HTTP/1.1 200 Connection Established")
 		_, err := connIn.Write(b)
 		if err != nil {
 			logger.Println("Write Connect err:", err)
@@ -82,6 +91,7 @@ func (hw *HandlerWrapper) Forward(resp http.ResponseWriter, req *http.Request, r
 		req.Header.Del("Proxy-Connection")
 		req.Header.Set("Connection", "Keep-Alive")
 		req.Header.Set("Origin", "http://"+raddr)
+
 		if err = req.Write(connOut); err != nil {
 			logger.Println("send to server err", err)
 			return
@@ -92,18 +102,16 @@ func (hw *HandlerWrapper) Forward(resp http.ResponseWriter, req *http.Request, r
 		log.Println("trans error ", err)
 	}
 }
-func connectProxyServer(conn net.Conn, addr string) error {
+func connectProxyServer(conn net.Conn, addr string, reqIn *http.Request) error {
 
 	req := &http.Request{
-		Method:     "CONNECT",
-		URL:        &url.URL{Host: addr},
+		Method:     reqIn.Method,
+		URL:        &url.URL{Host: addr, Path: reqIn.URL.Path},
 		Host:       addr,
-		ProtoMajor: 1,
-		ProtoMinor: 1,
+		RequestURI: reqIn.URL.Path,
 		Header:     make(http.Header),
 	}
-	req.Header.Set("Proxy-Connection", "keep-alive")
-	req.Header.Set("Origin", "http://"+addr)
+	//req.Header.Set("Proxy-Connection", "keep-alive")
 	if err := req.Write(conn); err != nil {
 		return err
 	}
